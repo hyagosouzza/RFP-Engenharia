1be3aa65a4501d2e1b2c5260ca21941b00dd0a8a
Hyago Souza <hyagosouzza@hotmail.com>
2018-09-18 14:24:30 -0300

    atualização

---
+++ src/app/app.routing.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LandingComponent } from './landing/landing.component';
+import { ServicosComponent } from './servicos/servicos.component';
+
+const routes: Routes =[
+    { path: 'home',             component: HomeComponent },
+    { path: 'user-profile',     component: ProfileComponent },
+    { path: 'landing',          component: LandingComponent },
+    { path: 'servicos',          component: ServicosComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
