27623d10869b7a6132a2077c44c614d0235c48e2
Hyago Vieira de Souza <hyagosouzza@hotmail.com>
2022-04-26 21:36:07 -0300

    Ajustes variáveis de ambiente site

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   emailjs: {
     apiUrl: 'https://api.emailjs.com/api/v1.0/email/send-form',
-    serviceId: '',
-    templateId: '',
-    userId: '',
+    serviceId: 'default_service',
+    templateId: 'template_ra6h4f9',
+    userId: 'OJGpkouDWb616TO0g',
   },
 };
